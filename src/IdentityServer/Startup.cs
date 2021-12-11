3d66728dd135835661439888aca9945cd886d81a
omrs2002 <omrs2002@yahoo.com>
2021-12-11 23:26:39 +0300

    Add project files.

---
+++ src/IdentityServer/Startup.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Reflection;
+using IdentityServer4.Configuration;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace IdentityServer
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; }
+        public Startup(
+            IWebHostEnvironment environment,
+            IConfiguration configuration
+            
+            )
+        {
+            Environment = environment;
+            Configuration = configuration;
+        }
+        public void ConfigureServices(
+            IServiceCollection services
+            )
+        {
+
+            services.AddSingleton(Configuration);
+
+
+            // uncomment, if you want to add an MVC-based UI
+            services.AddControllersWithViews();
+
+            string connectionString = Configuration.GetConnectionString("DefaultConnection");
+
+            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
+            var builder = services.AddIdentityServer(options =>
+            {
+                options.Events.RaiseErrorEvents = true;
+                options.Events.RaiseInformationEvents = true;
+                options.Events.RaiseFailureEvents = true;
+                options.Events.RaiseSuccessEvents = true;
+                options.UserInteraction.LoginUrl = "/Account/Login";
+                options.UserInteraction.LogoutUrl = "/Account/Logout";
+                options.Authentication = new AuthenticationOptions()
+                {
+                    CookieLifetime = TimeSpan.FromHours(10), // ID server cookie timeout set to 10 hours
+                    CookieSlidingExpiration = true
+                };
+            })
+            .AddConfigurationStore(options =>
+            {
+                options.ConfigureDbContext = b => b.UseSqlServer(connectionString, sql => sql.MigrationsAssembly(migrationsAssembly));
+            })
+            .AddOperationalStore(options =>
+            {
+                options.ConfigureDbContext = b => b.UseSqlServer(connectionString, sql => sql.MigrationsAssembly(migrationsAssembly));
+                options.EnableTokenCleanup = true;
+            });
+
+            //var builder = services.AddIdentityServer(options =>
+            //{options.EmitStaticAudienceClaim = true;})
+            //    .AddInMemoryIdentityResources(Config.IdentityResources)
+            //    .AddInMemoryApiScopes(Config.ApiScopes)
+            //    .AddInMemoryClients(Config.Clients);
+
+
+
+            // not recommended for production - you need to store your key material somewhere secure
+            builder.AddDeveloperSigningCredential();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            if (Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // uncomment if you want to add MVC
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            app.UseIdentityServer();
+
+            // uncomment, if you want to add MVC
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
